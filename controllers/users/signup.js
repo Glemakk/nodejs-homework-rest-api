1405a102b05c493dc44f3897521dc400b70edbc1
Glemakk <kovgar.brand@gmail.com>
2021-10-18 22:41:30 +0200

    verify doesn't work

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -2,9 +2,13 @@ const { Conflict } = require('http-errors');
 // const bcrypt = require("bcryptjs")
 const { User } = require('../../models');
 const gravatar = require('gravatar');
+const { v4 } = require('uuid');
+
+const {sendEmail} = require('../../helpers')
 
 const signup = async (req, res) => {
     const { email, password } = req.body;
+    console.log("email >>", email)
     const user = await User.findOne({ email });
     // 409 - already exist
     if (user) {
@@ -17,16 +21,26 @@ const signup = async (req, res) => {
         // return;
     }
 
-    var avatar = gravatar.url(email, {s: '250', d: 'robohash'}, true);
-    const newUser = new User({ email });
+    const avatar = gravatar.url(email, { s: '250', d: 'robohash' }, true);
+    const verifyToken = v4();
+    const newUser = new User({ email, verifyToken, avatar });
     //после объявления переменной - newUser это объект у кот-го есть св-во email
     // newUser = {email}
     newUser.setPassword(password);
-    newUser.setAvatar(avatar);
+    // newUser.setAvatar(avatar);
     //после объявления newUser.setPassword(password) - newUser это объект у кот-го есть св-во email и password
     // newUser = {email}
     await newUser.save(); //метод save - сохранили в базу
 
+    //создаем само письмо
+    const emailMsg = {
+        to: email,
+        subject: "Confirm new user sign-ups",
+        html: `
+        <a href="http://localhost:3000/users/verify/${verifyToken}" target="_blank">Confirm your email</a>`
+    }
+
+    await sendEmail(emailMsg);
     // const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
     // const newUser = {email, password: hashPassword}
     // await User.create(newUser);
@@ -38,7 +52,10 @@ const signup = async (req, res) => {
         // status: "success",
         // code: 201,
         // message: "Success signup"
+        //иногда отправляют еще verifyToken
+        //data: { verifyToken }
     })
+
 };
 
 module.exports = signup;
--- models/user.js
+++ models/user.js
@@ -28,7 +28,15 @@ const userSchema = Schema({
   },
   avatarURL: {
     type: String,
-  }
+  },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verifyToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+  },
 },
   { versionKey: false, timestamps: true},
 );
